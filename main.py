da36812936476c905302a22b18b40fbf861e046c
Meunier A. <123185224+Meunier6969@users.noreply.github.com>
2024-03-27 18:41:21 +0100

    bump

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from automata import Automata, parseAutomataFromFile
 
 def main():
-	auto: Automata = parseAutomataFromFile("machines/example_automata.txt")
+	# auto: Automata = parseAutomataFromFile("machines/example_automata.txt")
 	# auto: Automata = parseAutomataFromFile("machines/test_automata.txt")
 	# auto: Automata = parseAutomataFromFile("machines/deterministic_automata.txt")
 	# auto: Automata = parseAutomataFromFile("machines/complete_automata.txt")
+	auto: Automata = parseAutomataFromFile("machines/standard_automata.txt")
 	auto.display()
-	auto.isComplete(verbose=True)
+	# auto.isComplete(verbose=True)
+	auto.isStandard(verbose=True)
 
 if __name__=="__main__":
 	main()
