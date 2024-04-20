9177ccb6bfc978b2e64fd82e820e9113efb38978
An0 <ainine.nassim@gmail.com>
2024-04-20 12:46:30 +0200

    Un ptit os.system("cls") par ci par là
    C zoli

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from automata import Automata, parseAutomataFromFile
 import msvcrt
+import os
 
 def debug():
 	# auto: Automata = parseAutomataFromFile("machines/example_automata.txt")
@@ -24,15 +25,17 @@ def main():
 	#5.Minimize, 6.Complementary, 7.Read Word, 8.Go back
 	run1 = True
 	while run1 == True :
+		os.system("cls")
 		print("--- Efrei Finite Automata Project ---\nPress :\n1.Choose an automaton\n2.Quit")
 		inp1 = input("-> ")
 		match inp1:
 			case "1":
-				
+				os.system("cls")
 				print("--- To choose an automaton, please indicate which type of automaton you wish to use ---\n1.Test Automatons\n2.Given Automatons\n3.Newly Created Automatons")
 				inp2 = input ("-> ")
 				match inp2:
 					case "1":
+						os.system("cls")
 						print("--- Choose from these automatons ---\n1.exemple_automata\n2.test_automata\n3.deterministic_automata\n4.complete_automata\n5.standard_automata")
 						inp3 = input("-> ")
 						match inp3:
@@ -47,6 +50,7 @@ def main():
 							case "5" :
 								auto: Automata = parseAutomataFromFile("machines/standard_automata.txt")
 					case "2":
+						os.system("cls")
 						print("--- There are 44 automatons, from 1 to 44. ---\nEnter a number between 1 and 44")
 						inp4="0"
 						while int(inp4)<1 or int(inp4)>44:
@@ -60,14 +64,17 @@ def main():
 					
 				run2 = True
 				while run2 == True :
+					os.system("cls")
 					print("--- You have choosen an automata. What do you wish to with it ? ---\n1.Display\n2.Check\n3.Standardization\n4.Determinization\n5.Completion\n6.Minimize\n7.Complementary\n8.Quit")
 					inp5 = input("-> ")
 					match inp5 :
 						case "1" :
+							os.system("cls")
 							print("--- You choose to Display ---")
 							auto.display()
 							msvcrt.getch() #No idea if this works in Linux, check it yuiko
 						case "2" :
+							os.system("cls")
 							print("--- You choose to Check ---")
 							print("-> Is it Standard ?")
 							auto.isStandard(True)
@@ -77,6 +84,7 @@ def main():
 							auto.isComplete(True)
 							msvcrt.getch()
 						case "3" :
+							os.system("cls")
 							print("--- You choose to Standardize the automata ---")
 							if auto.isStandard() == True :
 								print("This automata is actually already Standard")
@@ -86,6 +94,7 @@ def main():
 								newAuto.display()
 								msvcrt.getch()
 						case "4" :
+							os.system("cls")
 							print("--- You choose to Determinize the automata ---")
 							if auto.isDeterministic() == True :
 								print("This automata is actually already Deterministic")
@@ -94,6 +103,7 @@ def main():
 								print("Not implemented yet, sorry")
 								msvcrt.getch()
 						case "5" : #Are there more checks to do ? I will need to rewatch the lesson I think
+							os.system("cls")
 							print("--- You choose to Complete the automata ---")
 							if auto.isComplete() == True :
 								print("This automata is actually already Complete")
@@ -103,9 +113,11 @@ def main():
 								newAuto.display()
 								msvcrt.getch()
 						case "6" : #Could be read word or Minimize here
+							os.system("cls")
 							print("--- You choose to Minimize the automata ---\nI don't think it will happen on this version")
 							msvcrt.getch()
 						case "7" :
+							os.system("cls")
 							print("--- You choose to make the Complementary of the automata ---")
 							newAuto = auto.complementary()
 							newAuto.display()
